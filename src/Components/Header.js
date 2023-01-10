f594c7b6f38255290f4aa9aa9ad571fad6df534d
Your Name <jlmeeks587@gmail.com>
2023-01-10 16:10:52 -0500

    styling

---
+++ src/Components/Header.js
@@ -0,0 +1,11 @@
+import { getDefaultNormalizer } from "@testing-library/react"
+
+const Header = () => {
+  return (
+    <header>
+      <h1>Task Tracker</h1>
+    </header>
+  )
+}
+
+export default Header
